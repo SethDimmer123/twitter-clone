62597ef81fc6c1bcb4897436612315a719951c1d
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-12 12:06:40 -0400

    initial commit

--- components/Trending.js
+++ components/Trending.js
@@ -1,6 +1,7 @@
 export default function Trending(){
+    // the trending tab needs to disappear on smaller screens 
     return(
-        <div>
+        <div className="hidden lg:flex">
             Trending
         </div>
     )
