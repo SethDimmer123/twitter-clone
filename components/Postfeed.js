bb5bde009f673d0f4ebab83b82d9cf26337722fb
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-12 13:30:45 -0400

    initial commit

--- components/Postfeed.js
+++ components/Postfeed.js
@@ -3,7 +3,7 @@ import TweetInput from "./TweetInput";
 
 export default function PostsFeed() {
     return (//flex-grow makes sure the feed takes up most of the space
-        <div className="sm:ml-20 xl:ml-96 max-w-2xl flex-grow
+        <div className="sm:ml-16 xl:ml-80 max-w-2xl flex-grow
         border-gray-700 border-x">
             {/* small breakpoint margin-left of 20
                 extra large breakpoint margin-left 96
