dc1863f448d40c343cfb4fbd6f359d024a1b860e
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-13 10:50:34 -0400

    initial commit

---
+++ components/BottomBanner.js
@@ -0,0 +1,19 @@
+export default function Bottombanner() {
+    return(
+        <div className="flex xl:space-x-[200px] justify-center items-center fixed w-full h-[80px] bg-[#1d9bf0] bottom-0">
+            <div className="hidden xl:inline text-white">
+                <h1 className="text-2xl font-bold">Don'miss what's happening</h1>
+                <span className="text-[18px] font-normal">People on Twitter are the first to know.</span>
+            </div>
+
+            <div className="space-x-3">
+                <button
+                className="bg-transparent border border-white text-white
+                w-[160px] rounded-full h-[40px] hover:bg-[#cbd2d7]">login</button>
+                <button 
+                className="bg-white  text-black
+                w-[160px] rounded-full h-[40px] hover:bg-[#cbd2d7]">Signup</button>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,30 @@ import styles from '@/styles/Home.module.css'
 import Sidebar from '@/components/Sidebar'
 import PostFeed from '@/components/Postfeed'
 import Trending from '@/components/Trending'
+import Bottombanner from '@/components/BottomBanner'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() { {
+export default function Home() {
+  {
 /* this div below is the parent of my components 
 that is why display flex works  */}
-  return (  
-    <div className="bg-black min-h-screen text-[#E7E9EA]
+  return (
+    <div>
+
+      <div className="bg-black min-h-screen text-[#E7E9EA]
     max-w-[1400px] mx-auto flex">
-      {/* mx-auto centers my bg-black also ml-auto mr-auto */}
-      {/* min-h-screen lets me have my entire background be black  */}
-      {/* if i increase the size of screen to 1440px  */}
-      {/* (39:32) the sidebar is not affected by flex because the sidebar has a position
+        {/* mx-auto centers my bg-black also ml-auto mr-auto */}
+        {/* min-h-screen lets me have my entire background be black  */}
+        {/* if i increase the size of screen to 1440px  */}
+        {/* (39:32) the sidebar is not affected by flex because the sidebar has a position
       of fixed if i have a position of fixed the component doesnt get affected by flex */}
-    <Sidebar/>
-    <PostFeed/>
-    <Trending/>
+        <Sidebar />
+        <PostFeed />
+        <Trending />
+      </div>
+
+      <Bottombanner />
     </div>
   )
 }
