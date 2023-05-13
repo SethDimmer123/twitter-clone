a88c865c729593cbe4920b4bffcecfcb412a8ca3
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-13 11:56:30 -0400

    initial commit

--- components/modals/SignUpModal.js
+++ components/modals/SignUpModal.js
@@ -6,19 +6,6 @@ import { useDispatch, useSelector } from "react-redux"
 export default function SignupModal() {
 
 
-// THE PROBLEM WITH USING USESTATE FOR MODALS IS I CAN ONLY CONTROL
-// MODAL FROM THE SIGNUP BUTTON AND NOWHERE ELSE.
-
-// I WANT TO CONTROL THE MODAL GLOBALLY(FROM ANYWHERE)
-// so i use redux and make the MODALS AVAILABLE GLOBALLY
-    // const [isOpen, setIsopen] = useState(false)
-    // if initially the isOpen is true then the onClose 
-    // will accept a function that turns the true intially to false
-    // const handleClose = () => setIsopen(false)
-
-    // const handleOpen = () => setIsopen(true)
-    //when i click the signUp button the modal pops up
-
     const isOpen = useSelector((state) => state.modals.signupModalOpen);
     // i am using the useDipsatch hook to use
     //  the handleClose functions i already made from useStates.
@@ -34,12 +21,6 @@ export default function SignupModal() {
                 //  to open the modal i have a function with a dispatch hook
                  >
                     Signup</button>
-                    {/* my modal is getting an error 
-                    because it needs props */}
-                    {/* open prop takes a boolean value
-                    onClose takes in a function and 
-                    changes the state of the open prop.
-                    */}
 
                     <Modal
                     
