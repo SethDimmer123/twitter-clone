98759e7903f4a0d98aac2f0f1b255bd812750cf5
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-17 12:05:28 -0400

    initial commit

--- components/Tweet.js
+++ components/Tweet.js
@@ -3,7 +3,13 @@ import { ChartBarIcon, ChatIcon, HeartIcon, UploadIcon } from "@heroicons/react/
 export default function Tweet({data}){
     return(
         <div className="border-b border-gray-700">
-            <TweetHeader username={data.username} name={data.name} timestamp={data.timestamp} text={data.tweet}/>
+            <TweetHeader 
+            username={data?.username} 
+            name={data?.name} 
+            // timestamp={data.timestamp} 
+            text={data?.tweet}
+            photoUrl={data?.photoUrl}
+            />
             <div className="p-3 ml-16 text-gray-500 flex space-x-14">
                 <ChatIcon className="w-5 cursor-pointer hover:text-green-400"/>
                 <HeartIcon className="w-5 cursor-pointer hover:text-pink-400"/>
@@ -15,17 +21,17 @@ export default function Tweet({data}){
 }
 
 // modifying my tweets so i can display the data this is after i worked on displaying the tweets on postfeed.js
-export function TweetHeader({username, name, timestamp,text}) {//accepting props
+export function TweetHeader({username, name, timestamp, text, photoUrl}) {//accepting props
     //custom component to reuse this component.
     //  exporting it so i can reuse it in other components.
     return(
         <div className="flex space-x-3 p-3  border-gray-700">
             <img className="w-11 h-11 rounded-full object-cover"
-             src="/assets/kylie.png"
+             src={photoUrl}
              />
             <div>
                 <div className="flex space-x-2 text-gray-500 items-center mb-1">
-                    <h1>{name}</h1>
+                    <h1 className="text-white font-bold">{name}</h1>
                     <span>@{username}</span>
                     <div className=" w-1 h-1 bg-gray-500 rounded-full"></div>
                     <span>{timestamp}</span>
